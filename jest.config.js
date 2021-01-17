257af1be1f8e16a8c885b15b4f4708dc0cf96280
||Nishant|| <thenishant3@gmail.com>
2021-01-17 23:02:13 +0530

    Nishant | setupFilesAfterEnv instead of global setup and teardown

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
     verbose: true,
     preset: 'jest-playwright-preset',
     testTimeout: 450000,
-    globalSetup: './tests/setup.ts',
-    globalTeardown: './tests/teardown.ts',
     transform: {
         '^.+\\.ts$': 'ts-jest',
     },
@@ -11,5 +9,7 @@ module.exports = {
     reporters: [
         "default",
         "jest-html-reporters"
-    ]
+    ],
+    testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 }
