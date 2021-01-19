64ad899925212f2c14e7cfab29f1f1d024e016bd
||Nishant|| <thenishant3@gmail.com>
2021-01-19 17:15:04 +0530

    Nishant | Renamed ProjectEdit class

--- src/pages/menuBarPages/ProjectEditPage.ts
+++ src/pages/menuBarPages/ProjectEditPage.ts
@@ -1,37 +1,39 @@
-import { Page } from 'playwright';
+import {Page} from 'playwright';
 
-export class ProjectEditpage {
+export class ProjectEditPage {
+
+    private page: Page
     private loadText = 'text=Naming, topics, avatar';
     private advancedSettings = 'id=js-project-advanced-settings';
     // private deleteProjectText = 'text=Delete project';
     private deleteProjectButton = 'xpath=//*[@id="js-project-advanced-settings"]/div[2]/div[6]/form/button';
     private deleteProjectWindowText = 'text=Delete project. Are you ABSOLUTELY SURE?';
-    private confirmProjectNameInput = 'id=confirm_name_input';
+    private confirmProjectNameInput = 'input#confirm_name_input.gl-form-input.form-control';
     private deletProjectButton = '#delete-project-modal-1___BV_modal_footer_ > button.btn.js-modal-action-primary.btn-danger.btn-md.gl-button > span'
     private projectText = '#delete-project-modal-1___BV_modal_body_ > div > p:nth-child(4) > code';
-    
-    async waitForPageLoad() {
-        await page.waitForSelector(this.loadText);
+
+    constructor(page: Page) {
+        this.page = page;
     }
 
     async clickAdvancedSettings() {
-        await page.$eval(this.advancedSettings, (element) => {
+        await this.page.$eval(this.advancedSettings, (element) => {
             element.scrollIntoView(false);
         });
-        await page.click(this.advancedSettings);
+        await this.page.click(this.advancedSettings);
     }
 
     async clickDeleteProject() {
-        await page.$eval(this.deleteProjectButton, (element) => {
+        await this.page.$eval(this.deleteProjectButton, (element) => {
             element.scrollIntoView(false);
         });
-        await page.click(this.deleteProjectButton);
+        await this.page.click(this.deleteProjectButton);
     }
 
-    async deleteProject(projectName1: string) {
-        let projectName = await page.innerText(this.projectText);
-        await page.waitForTimeout(1000);
-        await page.fill(this.confirmProjectNameInput, projectName);
-        await page.click(this.deletProjectButton);
+    async deleteProject() {
+        let projectName = await this.page.innerText(this.projectText);
+        await this.page.waitForTimeout(1000);
+        await this.page.type(this.confirmProjectNameInput, projectName);
+        await this.page.click(this.deletProjectButton);
     }
 }
